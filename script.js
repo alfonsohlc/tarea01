cd86c73e02b338a5f23f581272830eeffd1f9be3
alfonsohlc <alfonsocanabatehlc@gmail.com>
2024-10-18 10:47:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-  let color = false;
-  function cambiarColor() {
+let color = false;
+function cambiarColor() {
     const boton = document.getElementById("boton");
-      if (!color) {
-        boton.style.color = "black";
+    if (!color) {
+        boton.style.color = "black"; // Cambia el color a negro
         color = true;
-      } else {
-        boton.style.color = "white";
+    } else {
+        boton.style.color = "white"; // Cambia el color a blanco
         color = false;
-      }
-  }
+    }
+}
